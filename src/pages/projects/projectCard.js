2cc3519ed9138339740e09b059557e9f9f90dd14
maxwellmichael <maxwellmichael522@gmail.com>
2021-05-31 01:14:21 +0530

    Styled Projects Page

--- src/pages/projects/projectCard.js
+++ src/pages/projects/projectCard.js
@@ -7,8 +7,8 @@ const ProjectCard = (props)=>{
     return(
             <div className="project-card-main">
                 <div className="project-card-container" style={{backgroundImage:`url(${props.data.images?props.data.images[0].url:'#'})`}}>
-                    <div className="title">{props.data.title}</div>
                     <div className="subtitle">{props.data.location}</div>
+                    <div className="title">{props.data.title}</div>
                     <button onClick={()=>props.history.push({pathname:'/project', state:{...props.data}})} className="cta-button">View</button>
                     <div className="right-triangle"></div>
                 </div>
